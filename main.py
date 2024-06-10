961bf7b6520640b7a8f2912303152eb4b76b2419
Pavel Sapezhka <me@weastur.com>
2024-06-10 20:30:55 +0300

    upd

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import io
 import os
-import platform
 import sys
 
-if platform.system() != "Darwin":
+inp = input
+out = []
+if "LOCAL" not in os.environ:
     input_buffer = io.BytesIO(os.read(0, os.fstat(0).st_size))
     inp = lambda: input_buffer.readline().rstrip(b"\n").rstrip(b"\r")  # noqa
-else:
-    inp = input
-out = []
 
 
 
