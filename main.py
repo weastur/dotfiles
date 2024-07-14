8d9a8fe8af7f055a50d5d4d5ee391e39a40c9217
Pavel Sapezhko <pavel.sapezhko@gmail.com>
2024-07-14 22:54:29 +0300

    Revert "upd"
    This reverts commit 64c6e0b6a03e36d60c90672e142feadf0e76e705.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import io
 import os
 import sys
 
+inp = input
 out = []
 if "LOCAL" not in os.environ:
     input_buffer = io.BytesIO(os.read(0, os.fstat(0).st_size))
-    input = lambda: input_buffer.readline().rstrip(b"\n").rstrip(b"\r")  # noqa
+    inp = lambda: input_buffer.readline().rstrip(b"\n").rstrip(b"\r")  # noqa
 
 
 
