64c6e0b6a03e36d60c90672e142feadf0e76e705
Pavel Sapezhko <pavel.sapezhko@gmail.com>
2024-07-14 19:28:27 +0300

    upd

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import io
 import os
 import sys
 
-inp = input
 out = []
 if "LOCAL" not in os.environ:
     input_buffer = io.BytesIO(os.read(0, os.fstat(0).st_size))
-    inp = lambda: input_buffer.readline().rstrip(b"\n").rstrip(b"\r")  # noqa
+    input = lambda: input_buffer.readline().rstrip(b"\n").rstrip(b"\r")  # noqa
 
 
 
